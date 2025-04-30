71ca0df0ae57c4d292ffd24e6702699952e3e32e
Niharika <niharikathiriveedhi@gmail.com>
2025-04-30 20:41:59 +0530

    intial  commit

---
+++ main.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI, File, UploadFile
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+import requests
+from keras.layers import TFSMLayer
+import keras
+from tensorflow.keras.models import load_model
+
+# Use the proper endpoint name (usually 'serving_default')
+
+
+
+app = FastAPI()
+MODEL = load_model('./train/my_model.h5')
+
+
+
+
+
+
+CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+
+@app.get("/ping")
+async def ping():
+    return "Hello, I am alive"
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+@app.post("/predict")
+async def predict(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+
+    img_batch = np.expand_dims(image, 0)
+    predictions=MODEL.predict(img_batch)
+    predicted_class=CLASS_NAMES[np.argmax(predictions[0])]
+    confidence=np.max(predictions[0])
+    return {
+        'class':predicted_class,
+        'confidence':float(confidence)
+    }
+
+    
+
+    
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='localhost', port=8000)
+
+    
+
+
+
